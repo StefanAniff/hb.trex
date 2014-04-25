cc3b4c42053f880266f5473e614b3c958404dca8
Ian Verona Andersen <iva@d60.dk>
2014-04-25 09:30:55 +0200

    Wpf clickonce released

--- src/Trex.WpfClient/Trex.SmartClient/Properties/AssemblyInfo.cs
+++ src/Trex.WpfClient/Trex.SmartClient/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("T.Rex SmartClient")]
+[assembly: AssemblyTitle("T.Rex SmartClient H&B")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("d60")]
-[assembly: AssemblyProduct("Trex.SmartClient")]
+[assembly: AssemblyProduct("Trex.SmartClient H&B")]
 [assembly: AssemblyCopyright("Copyright © d60 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.15.3.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
